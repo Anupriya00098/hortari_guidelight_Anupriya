2598d57a165f2d7da11d1688d698591e4b1937d7
unknown <Administrator@SILAB-45.ASIETDomain.com>
2020-02-18 12:26:14 +0530

    new file

--- src/App.js
+++ src/App.js
@@ -2,27 +2,43 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { render } from '@testing-library/react';
+import productTile from './components/productTile';
+
+const productArray =[
+    {
+        "id":1,
+        "title":"oneplus7",
+        "price":"Rs 30000"
+    },
+    {
+        "id":2,
+        "title":"oppo",
+        "price":"Rs 12000"
+    },
+    {
+        "id":3,
+        "title":"m30s",
+        "price":"Rs 16000"
+    }
+]
 class App extends React.Component {
     constructor(props){
         super(props);
         this.state={
             firstState: 'hello world',
-            cartCount: 0
+            cartCount: 0,
+            products:[] 
         }
     }
         
-
-
-    componentDidMount(){
-        this.setState({
-            firstState: 'Welcome to our shop'
-        })
-    }
+    
     componentDidUpdate(PrevProps,preState){
         if(this.state.cartCount>=10)
         alert('cannot add more than 10 items')
     }
-    
+    componentDidMount(){
+        this.loadProducts()
+    }
     addCount(){
       const newCount=this.state.cartCount+1;
       this.setState({
@@ -31,13 +47,21 @@ class App extends React.Component {
       })
       
     }
-
+ loadProducts(){
+     this.setState({
+         products:productArray
+     })
+ }
  render() {
+     const productList=this.state.products.map((products)=>{
+         return productTile(this.addCount.bind(this),products)
+     })
+     console.log(productList)
     return (
    
         <div className="container">
           <nav className="navbar navbar-light" style={{"backgroundColor": "lavender"}}>
-    
+          
     <img src="download.jpg." width="70" height="150" className="d-inline-block align-top" alt=""/>
     {this.state.firstState}
     
@@ -45,26 +69,8 @@ class App extends React.Component {
     <button type="button" className="btn btn-warning">Cart{this.state.cartCount}</button>
     </p>
     </nav>
-            <div className="row">
-                <div className="col-3">
-                    <div className="card mb-3" style={{"maxWidth": "540px"}}>
-                        <div className="row no-gutters">
-                            <div className="col-md-4">
-                                <img src="1.jpg" className="card-img" alt="..."/>
-                            </div>
-                            <div className="col-md-8">
-                                <div className="card-body">
-                                    <h5 className="card-title">Item</h5>
-                                    <button type="button" className="btn btn-primary btn-lg" onClick={() =>{
-                                      this.addCount()
-                                      
-                                    }
-
-                                    }>ADD</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+               <div className="row">
+                {productList}
                 </div>
                 
             <p align="right">
@@ -74,7 +80,7 @@ class App extends React.Component {
 
                                     }>Checkout</button></p>
     
-    </div>
+    
     </div>
     
     
---
+++ src/components/productTile.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+export default function productTile(addCount,products) {
+    return (
+       
+        <div className="col-3">
+            <div className="card mb-3" style={{"maxWidth": "540px"}}>
+                <div className="row no-gutters">
+                    <div className="col-md-4">
+                        <img src="1.jpg" className="card-img" alt="..."/>
+                    </div>
+                    <div className="col-md-8">
+                        <div className="card-body">
+                            <h5 className="card-title">{products.title}</h5>
+                            <h5>{products.price}</h5>
+                            <button type="button" className="btn btn-primary btn-lg" onClick={() =>{
+                              addCount()
+                              
+                            }
+
+                            }>ADD</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+)
+}
